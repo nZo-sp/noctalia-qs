73f96fbdfd894c958fffba6150ef804e2e5571f6
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-03-11 09:55:20 +0100

    chore(nix): add nix-treefmt (with nixfmt, deadnix & statix)
    same linters/formatter noctalia-shell uses. nix-treefmt is just for
    convenience so you don’t have to run them manually for each file.

--- flake.nix
+++ flake.nix
@@ -4,44 +4,52 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    systems,
-    ...
-  }: let
-    eachSystem = fn:
-      nixpkgs.lib.genAttrs
-      (import systems)
-      (system: fn nixpkgs.legacyPackages.${system});
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      treefmt-nix,
+      ...
+    }:
+    let
+      eachSystem =
+        fn: nixpkgs.lib.genAttrs (import systems) (system: fn nixpkgs.legacyPackages.${system});
 
-    mkDate = longDate:
-      nixpkgs.lib.concatStringsSep "-" [
-        (builtins.substring 0 4 longDate)
-        (builtins.substring 4 2 longDate)
-        (builtins.substring 6 2 longDate)
-      ];
+      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./nix/treefmt.nix);
 
-    version = mkDate (self.lastModifiedDate or "19700101") + "_" + (self.shortRev or "dirty");
-    gitRev = self.rev or self.dirtyRev or "dirty";
-  in {
-    overlays.default = final: prev: {
-      quickshell = final.callPackage ./nix/package.nix {inherit version gitRev;};
-    };
+      mkDate =
+        longDate:
+        nixpkgs.lib.concatStringsSep "-" [
+          (builtins.substring 0 4 longDate)
+          (builtins.substring 4 2 longDate)
+          (builtins.substring 6 2 longDate)
+        ];
 
-    packages = eachSystem (pkgs: {
-      quickshell = pkgs.callPackage ./nix/package.nix {inherit version gitRev;};
-      default = self.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
-    });
-
-    devShells = eachSystem (pkgs: {
-      default = pkgs.callPackage ./nix/shell.nix {
-        quickshell = self.packages.${pkgs.stdenv.hostPlatform.system}.quickshell.override {
-          stdenv = pkgs.clangStdenv;
-        };
+      version = mkDate (self.lastModifiedDate or "19700101") + "_" + (self.shortRev or "dirty");
+      gitRev = self.rev or self.dirtyRev or "dirty";
+    in
+    {
+      overlays.default = final: _prev: {
+        quickshell = final.callPackage ./nix/package.nix { inherit version gitRev; };
       };
-    });
-  };
+
+      packages = eachSystem (pkgs: {
+        quickshell = pkgs.callPackage ./nix/package.nix { inherit version gitRev; };
+        default = self.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
+      });
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.callPackage ./nix/shell.nix {
+          quickshell = self.packages.${pkgs.stdenv.hostPlatform.system}.quickshell.override {
+            stdenv = pkgs.clangStdenv;
+          };
+        };
+      });
+
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+    };
 }
